525b2bf9dffc4b9762e717bbf2bade6f6e26f62e
ignacio <ignacio.moenen@gmail.com>
2022-06-28 12:36:28 -0400

    nuevo proyecto

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import img1 from '../imgs/tetris-reactjs.jpg'
 import img2 from '../imgs/bodeapp.jpg'
+import img3 from '../imgs/finanzas-react.png'
 
 const Card = () => {
   return (
@@ -26,6 +27,19 @@ const Card = () => {
                                     <p>Pass: 123456</p>
                                     <a target='_blank' rel="noreferrer nopener" href="https://proyecto-fullstack-bodeapp.herokuapp.com/" className="btn btn-outline-primary">Probar</a>
                                     <a target='_blank' rel="noreferrer nopener" href="https://github.com/Sepjir/Proyecto_Fullstack_BodeApp" className="btn btn-outline-success mx-1">Repositorio</a>
+                                </div>
+                        </div>
+
+                        <div className="card col-12 col-md-4 mx-auto mt-3 ">
+                            <img src={img3} className="card-img-top img-fluid text-center mt-1" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Finanzas Personales</h5>
+                                    <p className="card-text">Aplicación financiera que lleva registros de <strong>Ingresos </strong> y <strong>Egresos</strong> de dinero, y un balance con registro de usuarios <strong> La aplicación web está construida en ExpressJS, ReactJS y PostgreSQL para la persistencia de datos</strong>, y que presenté como prueba para Alkemy</p>
+                                    <p>Para iniciar sesión utilizar las siguientes credenciales:</p>
+                                    <p>Mail: invitado@invitado.cl</p>
+                                    <p>Pass: 123456</p>
+                                    <a target='_blank' rel="noreferrer nopener" href="https://sensational-quokka-997c04.netlify.app/" className="btn btn-outline-primary">Probar</a>
+                                    <a target='_blank' rel="noreferrer nopener" href="https://github.com/Sepjir/alkemy-challenge" className="btn btn-outline-success mx-1">Repositorio</a>
                                 </div>
                         </div>
 
